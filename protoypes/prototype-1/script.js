f7626432a21ba6eabeddb4276747351950a9abb6
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-01-07 09:52:30 +0000

    Add BLO class

--- protoypes/prototype-1/script.js
+++ protoypes/prototype-1/script.js
@@ -1,26 +1,21 @@
+var work = new Work();
+var workBLO = new WorkBLO();
+
+
+
+
 document.getElementById("formSubmit").addEventListener("submit", function (event) {
     event.preventDefault();
-    var work = readwork();
+    readwork()
     insertNewRow(work);
+    workBLO.addWork(work);
 })
 
 
 function readwork() {
-
-    var work = {};
-    work["title"] = document.getElementById("inputTitle").value;
-    work["author"] = document.getElementById("inputAuthor").value;
-    work["price"] = parseFloat(document.getElementById("inputPrix").value);
-    work["date"] = document.getElementById("inputDate").value;
-    work["language"] = document.getElementById("inputLanguage").value;
-    var cheackValues = document.getElementsByName("workType");
-    for (var i = 0; i < cheackValues.length; i++) {
-        if (cheackValues[i].checked) {
-            work["type"] = cheackValues[i].value;
-            break;
-        }
-    }
-    return work;
+    work.id = workBLO.counter++;
+    work.title = document.getElementById("inputTitle").value;
+    
 }
 
 
@@ -28,18 +23,10 @@ function readwork() {
 function insertNewRow(work) {
     var tableBody = document.getElementById("worksTable").getElementsByTagName('tbody')[0];
     var newRow = tableBody.insertRow(tableBody.length);
-    newRow.insertCell(0).innerHTML = work.title;
+    newRow.insertCell(0).innerHTML = work.id;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = work.author;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = work.price;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = work.date;
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = work.language
-    cell6 = newRow.insertCell(5)
-    cell6.innerHTML = work.type
-    cell7 = newRow.insertCell(6)
+    cell2.innerHTML = work.title;
+ 
 
 
 }
---
+++ protoypes/prototype-1/workBLO.js
@@ -0,0 +1,12 @@
+class WorkBLO {
+    workList;
+    counter = 0;
+
+    addWork(work) {
+        this.workList.push(work)
+    }
+
+    editWork(work) {
+        
+    }
+}
