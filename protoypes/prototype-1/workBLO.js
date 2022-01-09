acc44c9791ffec88df79e0730e1f40e8254db1e4
maskoul-mohamed <moha1058@gmail.com>
2022-01-09 15:25:24 +0100

    Add edit functionality

--- protoypes/prototype-1/workBLO.js
+++ protoypes/prototype-1/workBLO.js
@@ -13,6 +13,7 @@ class WorkBLO {
     }
 
     editWork(work) {
+        console.log(work)
         for (var i = 0; i < this.#workList.length; i++) {
             if (work.id == this.#workList[i].id) {
                 this.#workList[i] = work
@@ -34,5 +35,18 @@ class WorkBLO {
        this.#workList =  this.#workList.filter(function(work){
             return work.id != id
         })
+    }
+
+
+    getAllItems() {
+        return this.#workList.sort(function(a, b) {
+            return a.title.localeCompare(b.title)
+        })
+        // return this.#workList.sort(function (a, b) {
+        //     if(a.title > b.title){ return -1}
+        //     if(a.title < b.title){ return 1}
+
+        //     return 0
+        // })
     }
 }
