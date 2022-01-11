4ae6dbd1f59d858cb931f0465d99a4bb5e4d2b19
maskoul-mohamed <moha1058@gmail.com>
2022-01-11 21:40:52 +0100

    Edit code to work local storege

---
+++ protoypes/prototype-2/work.js
@@ -0,0 +1,20 @@
+class Work {
+    id;
+    title;
+
+//   get id(){
+//       return this.id;
+//   }
+  
+//   set id(value){
+//       this.id = value;
+//   }
+
+//   get title(){
+//       return this.title;
+//   }
+
+//   set title(value){
+//       this.title = value;
+//   }
+}
