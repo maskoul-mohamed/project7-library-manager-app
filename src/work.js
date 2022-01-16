792644827d2c5f9498f8496fb23db0a233d20d99
maskoul-mohamed <moha1058@gmail.com>
2022-01-16 18:36:47 +0100

    Add work and workmanger files

---
+++ src/work.js
@@ -0,0 +1,25 @@
+class Work {
+    id;
+    title;
+    author;
+    price;
+    date;
+    language;
+    type;
+
+//   get id(){
+//       return this.id;
+//   }
+  
+//   set id(value){
+//       this.id = value;
+//   }
+
+//   get title(){
+//       return this.title;
+//   }
+
+//   set title(value){
+//       this.title = value;
+//   }
+}
